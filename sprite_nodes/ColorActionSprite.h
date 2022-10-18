c5817c96d359a51816f37e99787880dfa77465b6
ConfiG <cgytrus@cgyt.ru>
2022-10-18 17:54:14 +0300

    oops

--- sprite_nodes/ColorActionSprite.h
+++ sprite_nodes/ColorActionSprite.h
@@ -5,6 +5,7 @@
 
 namespace gd {
     class GDH_DLL ColorActionSprite : public cocos2d::CCNode {
+    public:
         float m_opacity;
         cocos2d::ccColor3B m_f0124;
         cocos2d::ccColor3B m_activeColor;
